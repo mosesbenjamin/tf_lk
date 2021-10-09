6a88b5882dd0d7f88d9875234e4e8808136b5b2e
fanalter1tech <fanalter1tech@gmail.com>
2021-10-09 06:27:33 +0100

    refactor to use asg

--- prod.tf
+++ prod.tf
@@ -57,35 +57,8 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count         = 2
-
-  ami           = "ami-5444e45adr5"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-   instance_id   = aws_instance.prod_web.0.id
-   allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }  
-}
-
 resource "aws_elb" "prod_web" {
   name            = "prod-web"
-  instances       = aws_instance.prod_web.*.id
   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -96,7 +69,41 @@ resource "aws_elb" "prod_web" {
     lb_protocol       = "http"
   }
 
-    tags = {
+  tags = {
     "Terraform" : "true"
   } 
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-1a2b3c676"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones  = ["us-west-2a", "us-west-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Terraform" 
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.asg.id
+  elb                    = aws_elb.prod_web.id
 }
