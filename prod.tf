969f22d9001adc97975af53a0bf3e1600d2b9670
fanalter1tech <fanalter1tech@gmail.com>
2021-10-09 05:33:32 +0100

    adopt default vpc and rename tf file to prod

--- prod.tf
+++ prod.tf
@@ -3,7 +3,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-linkedn-course-20210910"
   acl    = "private"
 }
+
+
+resource "aws_default_vpc" "default" {}
+
+
