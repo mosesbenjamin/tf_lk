e31949f96d9b25658df5e0f79c12c8958a88fddf
fanalter1tech <fanalter1tech@gmail.com>
2021-10-09 06:14:56 +0100

    load balancer for prod web

--- prod.tf
+++ prod.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
@@ -10,6 +10,22 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+
+    tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+
+    tags = {
+    "Terraform" : "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
@@ -42,7 +58,7 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
-  count = 2
+  count         = 2
 
   ami           = "ami-5444e45adr5"
   instance_type = "t2.nano"
@@ -57,7 +73,7 @@ resource "aws_instance" "prod_web" {
 }
 
 resource "aws_eip_association" "prod_web" {
-   instance_id = aws_instance.prod_web.0.id
+   instance_id   = aws_instance.prod_web.0.id
    allocation_id = aws_eip.prod_web.id
 }
 
@@ -65,4 +81,22 @@ resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
   }  
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+    tags = {
+    "Terraform" : "true"
+  } 
 }
